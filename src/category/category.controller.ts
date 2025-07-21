d93d98aa75a7e4ca886e73d8f49d64dd2499685b
thersvg <rodrigohend@outlook.com>
2025-07-21 19:22:57 -0400

    up

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -12,21 +12,23 @@ import { CategoryDTO } from './dtos/category.dto';
 import { CategoryService } from './category.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 
-@UseGuards(AuthGuard)
 @Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   async create(@Body() data: CategoryDTO) {
     return await this.categoryService.create(data);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   async update(@Param('id') id: number, @Body() data: CategoryDTO) {
     return await this.categoryService.update(Number(id), data);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: number) {
     return await this.categoryService.remove(Number(id));
